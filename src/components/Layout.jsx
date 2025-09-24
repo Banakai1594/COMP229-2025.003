598e63e4c0e5a01b2cbe8bf30b4428dbed720874
Banakai1594 <amusta42@my.centennialcollege.ca>
2025-09-24 16:17:54 -0400

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom";
-import logo from "../assets/logo.jpeg";
+import logo from "../assets/logo.png";
 import "./layout.css";
 
 export default function Layout() {
